9b5f9f312601d389b86d2c762657220dcd9a6dd9
PeterEhses <ehsesp@hochschule-trier.de>
2020-07-07 11:09:20 +0200

    ???

---
+++ src/elements/Container/Container.vue
@@ -0,0 +1,63 @@
+<template>
+  <section :class="type">
+    <slot>content</slot>
+    <p>hi!</p>
+  </section>
+</template>
+
+<script>
+/**
+ * the card container is an empty canvas for any card related components
+ */
+export default {
+  name: "Container",
+  status: "prototype",
+  release: "0.0.1",
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: "card",
+      validator: function(value) {
+        return ["card", "outline"].indexOf(value) !== -1
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  min-width: $card-min-width;
+  min-height: $card-min-height;
+  background-color: $offwhite;
+  margin: $space-s;
+  border-radius: $radius-s;
+  box-shadow: $shadow-light;
+}
+
+.outline {
+  box-sizing: border-box;
+  background-color: rgba(0, 0, 0, 0);
+  border: $space-s solid $gray2;
+  border-radius: $radius-m;
+}
+</style>
+
+<docs>
+
+### Container Type:
+
+  ```jsx
+  <div>
+    <Container type="card"/>
+  </div>
+  ```
+  ### Outline Type:
+
+  ```jsx
+  <div>
+    <Container type="outline"/>
+  </div>
+  ```
+</docs>
